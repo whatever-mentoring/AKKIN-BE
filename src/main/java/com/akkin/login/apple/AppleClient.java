b766724aaf6b1ecf050b58eb6ae8b3fabff9245b
brorica <minjea917@gmail.com>
2023-09-23 16:18:43 +0900

    refact: 안 쓰는 의존성 삭제

--- src/main/java/com/akkin/login/apple/AppleClient.java
+++ src/main/java/com/akkin/login/apple/AppleClient.java
@@ -4,7 +4,7 @@ import com.akkin.login.apple.dto.ApplePublicKeys;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(url = "https://appleid.apple.com")
+@FeignClient(name = "apple-public-key", url = "https://appleid.apple.com")
 public interface AppleClient {
 
     @GetMapping("/auth/keys")
