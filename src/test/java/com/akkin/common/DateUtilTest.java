fe3e994361176a191c62c23eb8c4bd5929765775
권민재 <minjea917@gmail.com>
2023-10-30 17:04:14 +0900

    세부 테스트 추가

--- src/test/java/com/akkin/common/DateUtilTest.java
+++ src/test/java/com/akkin/common/DateUtilTest.java
@@ -28,7 +28,7 @@ public class DateUtilTest extends UnitTest{
     }
 
     @Test
-    public void 입력된_날짜가_최신_주면_현재_주_정보를_보여줌() {
+    public void 최신_주면_현재_주_정보를_보여줌() {
         // given
         LocalDate now = LocalDate.now();
         LocalDate startOfWeek = now.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
@@ -40,5 +40,35 @@ public class DateUtilTest extends UnitTest{
         // then
         assertThat(weekOfMonth.getStartOfNextWeek()).isEqualTo(startOfWeek);
         assertThat(weekOfMonth.getEndOfNextWeek()).isEqualTo(endOfWeek);
+    }
+
+    @Test
+    public void 다음날이_달이_바뀌는_경우() {
+        // given
+        LocalDate day = LocalDate.of(2023, 8, 31);
+
+        // when
+        MonthWeekInfo weekOfMonth = dateUtil.getWeekOfMonth(day);
+
+        // then
+        assertThat(weekOfMonth.getStartOfPrevWeek()).isEqualTo(LocalDate.of(2023, 8, 20));
+        assertThat(weekOfMonth.getEndOfPrevWeek()).isEqualTo(LocalDate.of(2023, 8, 26));
+        assertThat(weekOfMonth.getStartOfNextWeek()).isEqualTo(LocalDate.of(2023, 9, 3));
+        assertThat(weekOfMonth.getEndOfNextWeek()).isEqualTo(LocalDate.of(2023, 9, 9));
+    }
+
+    @Test
+    public void 오늘_달이_바뀐_경우() {
+        // given
+        LocalDate day = LocalDate.of(2023, 9, 1);
+
+        // when
+        MonthWeekInfo weekOfMonth = dateUtil.getWeekOfMonth(day);
+
+        // then
+        assertThat(weekOfMonth.getStartOfPrevWeek()).isEqualTo(LocalDate.of(2023, 8, 20));
+        assertThat(weekOfMonth.getEndOfPrevWeek()).isEqualTo(LocalDate.of(2023, 8, 26));
+        assertThat(weekOfMonth.getStartOfNextWeek()).isEqualTo(LocalDate.of(2023, 9, 3));
+        assertThat(weekOfMonth.getEndOfNextWeek()).isEqualTo(LocalDate.of(2023, 9, 9));
     }
 }
